b65f738dce6221786e7cba6ff440ebd01715c446
Veronika Laippala <mavela@biarc.utu.fi>
2021-08-19 17:10:50 +0300

    updates to code

--- analyze.py
+++ analyze.py
@@ -31,47 +31,65 @@ def most_frequent(data, col, how_many,*focus):
                 continue
         else:
                 line=line.split("\t") # split to list
-                if len(focus) == 0: # if 0, we 
+                if line[cols.index("UPOS")] == "PUNCT": # let's ignore punctuation
+                        continue
+                elif len(focus) == 0: # if 0, we take all, except for punctuation
                     my_counter[line[cols.index(col)]] +=1
-                else:
-#                    print(line[cols.index(col)],"".join(focus))
-                    if line[cols.index(col)] == "".join(focus):
+                elif line[cols.index(col)] == "".join(focus): #if focus specifies a group we want to focus on, it is defined here
                          my_counter[line[2]] +=1 #take lemmas
     for word, count in my_counter.most_common(how_many):
         result = result + word + " " + str(count) + "\n"
     return(result)
- 
+
+#print(most_frequent(sys.argv[1], "LEMMA", 10))
+
 def read_text(inp):
     id = None
     text = []
     for line in inp:
-        line=line.strip()
-        if line.startswith("# <doc id"):
-            if id != None:
-                yield(register, text)
-                text = []
-                register = None
-                id = line
-            elif id == None:
+            line=line.strip()
+            if line.startswith("# <doc id"):
+                if id != None:
+                    yield(register, text)
+                    text = []
+                    register = None
+                    id = line
+                elif id == None:
                     id=line
-        else:
-            if line.startswith("#"):
-                if line.startswith("# predicted register"):
-                    register=line.split(":")[1].strip()
-                else:
-                    continue
+            elif line.startswith("# predicted register"):
+                        register=line.split(":")[1].strip()
             elif not line.startswith("#"):
-                text.append(line)
+                if len(line)== 0:
+                    continue
+                else:
+                    text.append(line.split("\t"))
+            else:
+                continue
     yield(register, text)
 
 def extract_register(register, data):
-    file_out = open(register + "_ext_" + "conllu.gz", "w")
+    file_out = open(register + "_ext" + ".conllu", "w")
     for reg, text  in read_text(open_f(data)):
         if reg == register :
-            file_out.write("# register: " + register + "\n")
-            file_out.write("\n".join(text))
+            file_out.write("# predicted register: " + register + "\n")
+            for line in text:
+                if line[0] == "1":
+                    print("\n")
+                    file_out.write("\n")
+                file_out.write("\t".join(line)+"\n")
             file_out.write("\n")
     file_out.close()
     print("Wrote", register, "texts to a file!")
     return(file_out)
 
+extract_register("narrative", sys.argv[1])
+def print_text(data, col):
+    cols = ["ID","FORM","LEMMA","UPOS","XPOS","FEAT","HEAD","DEPREL","DEPS","MISC"] #the columns of the conllu format
+    to_return = []
+    for reg, text in read_text(open_f(data)):
+            words = (word_line[cols.index(col)] for word_line in text)
+            print(" ".join(words))
+    return(to_return)
+
+
+        
